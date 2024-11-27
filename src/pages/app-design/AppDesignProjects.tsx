e1f73c054f102b6aee00818ec8fc0edc93fd0ba9
Dev <97352849+Vicdiamond@users.noreply.github.com>
2024-11-28 00:44:28 +0100

    fixed index

--- src/pages/app-design/AppDesignProjects.tsx
+++ src/pages/app-design/AppDesignProjects.tsx
@@ -5,8 +5,8 @@ function AppDesignProjects() {
   const appDesignProjectsInfo = useDesignProjectsDetails();
   return (
     <div className="xl:grid xl:grid-cols-3 gap-[30px] items-center mx-auto justify-between mt-[96px] space-y-8 xl:space-y-0 ">
-      {appDesignProjectsInfo.map((project) => (
-        <ProjectGalleryItem project={project} key={project.title} />
+      {appDesignProjectsInfo.map((project, i) => (
+        <ProjectGalleryItem project={project} key={project.title} i={i} />
       ))}
     </div>
   );
--- src/pages/graphic-design/GraphicDesignProjects.tsx
+++ src/pages/graphic-design/GraphicDesignProjects.tsx
@@ -5,8 +5,8 @@ function GraphicDesignProjects() {
   const graphicDesignProjectsInfo = useDesignProjectsDetails();
   return (
     <div className="xl:grid xl:grid-cols-3 gap-[30px] items-center mx-auto justify-between mt-[96px] space-y-8 xl:space-y-0 ">
-      {graphicDesignProjectsInfo.map((project) => (
-        <ProjectGalleryItem project={project} key={project.title} />
+      {graphicDesignProjectsInfo.map((project, i) => (
+        <ProjectGalleryItem project={project} key={project.title} i={i} />
       ))}
     </div>
   );
